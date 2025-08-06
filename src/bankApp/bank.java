a70726ccb62e6c99db148866daeccdbca21df201
Cjunabia2c <christianjayunabia7@gmail.com>
2025-08-06 23:07:27 +0800

    added activity5

---
+++ src/bankApp/bank.java
@@ -0,0 +1,26 @@
+package bankApp;
+import java.util.Scanner;
+public class bank {
+    int accountNo = 10001;
+    String name;
+    float initialBalance = 0;
+    int pin = 1234;
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public int setAccount(){
+        return accountNo;
+    }
+    
+    public void viewBalance(){
+    
+    
+    }
+}
